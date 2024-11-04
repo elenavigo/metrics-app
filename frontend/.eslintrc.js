db2e993430ac069a1ff99a030be9d863d30d3379
Elena <elenavigoolivan@gmail.com>
2024-11-04 16:35:11 +0100

    Add some linting and prettier on save

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,22 +1,28 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    rules: {
-        'react/react-in-jsx-scope': 'off',
-    },
+  },
+  plugins: ['prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
