f2b6b16d1197018ad9d653e6c5e6dd6a159a243a
Elena <elenavigoolivan@gmail.com>
2024-11-04 17:46:56 +0100

    Add metric form

---
+++ frontend/src/components/metric-form.tsx
@@ -0,0 +1,128 @@
+import * as yup from 'yup';
+import { MetricProperties } from '../interfaces/metric';
+import { FC, useState } from 'react';
+
+interface Props {
+  onSubmit: (properties: MetricProperties) => void;
+}
+
+export const MetricForm: FC<Props> = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    value: '',
+    date: new Date().toISOString().substring(0, 10),
+  });
+
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+  const validationSchema = yup.object().shape({
+    name: yup.string().required(),
+    value: yup.string().required(),
+    date: yup.date().required(),
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('ff');
+    try {
+      await validationSchema.validate(formData, { abortEarly: false });
+      setErrors({});
+      onSubmit({ ...formData, date: new Date(formData.date) });
+    } catch (error) {
+      console.log(error);
+      if (error instanceof yup.ValidationError) {
+        const validationErrors: { [key: string]: string } = {};
+        error.inner.forEach((err) => {
+          if (err.path) {
+            validationErrors[err.path] = err.message;
+          }
+        });
+        setErrors(validationErrors);
+      }
+    }
+  };
+
+  return (
+    <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="name"
+          >
+            Name
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-300"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="value"
+          >
+            Value
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-300"
+            type="text"
+            name="value"
+            value={formData.value}
+            onChange={handleChange}
+          />
+          {errors.value && <p style={{ color: 'red' }}>{errors.value}</p>}
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="date"
+          >
+            Date
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            type="date"
+            className=" border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-300"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+          {errors.date && <p style={{ color: 'red' }}>{errors.date}</p>}
+        </div>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            type="submit"
+          >
+            Add metric
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { LineChart } from '../components/line-chart';
 import { useMetrics } from '../hooks/use-metrics';
+import { MetricForm } from '../components/metric-form';
+import { MetricProperties } from '../interfaces/metric';
 
 export const Dashboard: React.FC = () => {
-    const { metrics } = useMetrics()
+  const { metrics } = useMetrics();
 
-    console.log(metrics)
+  console.log(metrics);
 
-    return (<div> <h1>Home Page</h1> <LineChart /></div>);
+  const onSubmit = (properties: MetricProperties) => {
+    console.log(properties);
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <LineChart />
+      <MetricForm onSubmit={onSubmit} />
+    </div>
+  );
 };
